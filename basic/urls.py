aa9a10203cf84a983be49286a889dbeb1fa18674
gulbaz-laptop <mgulbazilyas@gmail.com>
2024-05-20 01:54:25 +0500

    done frontend task

--- basic/urls.py
+++ basic/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from basic import views
-appname = 'basic'
+
+app_name = 'basic'
 
 urlpatterns = [
     path('', views.Homepage.as_view(), name='home'),
+    path('task-1/', views.Task1.as_view(), name='task-1'),
+    path('task-2/', views.Task2.as_view(), name='task-2'),
+    path('task-3/', views.Task3.as_view(), name='task-3'),
+    path('api/weather/<int:value>/', views.apiWeather, name='api-weather'),
     path('mechanical/<str:equipment>/', views.mechanical, name='mechanical'),
+    path('mechanical/<str:equipment>/<int:fault>/', views.mechanical, name='mechanical-fault'),
 ]
--- basic/views.py
+++ basic/views.py
@@ -6,17 +6,45 @@ class Homepage(TemplateView):
     template_name = 'index.html'
 
 
+class Task1(TemplateView):
+    template_name = 'task-1.html'
+
+
+class Task2(TemplateView):
+    template_name = 'task-2.html'
+
+
+class Task3(TemplateView):
+    template_name = 'index.html'
+
+
 # Create your views here.
 
-def mechanical(request, equipment):
+def mechanical(request, equipment, fault=0):
 
     return JsonResponse([
-    {"Equipment":f"{equipment}-L01-01", "Status":1, "Fault":0},
-    {"Equipment":f"{equipment}-L01-02", "Status":1, "Fault":0},
-    {"Equipment":f"{equipment}-L01-03", "Status":1, "Fault":0},
-    {"Equipment":f"{equipment}-L01-04", "Status":0, "Fault":1},
-    {"Equipment":f"{equipment}-L01-05", "Status":1, "Fault":0},
-    {"Equipment":f"{equipment}-L01-06", "Status":0, "Fault":0},
-    {"Equipment":f"{equipment}-L01-07", "Status":1, "Fault":0}
+            {"Equipment": f"555CS-B-MECH-L02-FCU-E1/{equipment}-INT-E1", "Status": 1, "Fault": 0, "Fan Speed": "36%", "SA Temperature": "30.6°C", "Last Updated": "16/05/2024 01:53 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-E2/{equipment}-INT-E2", "Status": 1, "Fault": 0, "Fan Speed": "66%", "SA Temperature": "32.4°C", "Last Updated": "16/05/2024 01:54 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-E3/{equipment}-INT-E3", "Status": 1, "Fault": 0, "Fan Speed": "56%", "SA Temperature": "32.2°C", "Last Updated": "16/05/2024 01:54 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-N1/{equipment}-INT-N1", "Status": 1, "Fault": 0, "Fan Speed": "56%", "SA Temperature": "32.6°C", "Last Updated": "16/05/2024 01:54 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-N2/{equipment}-INT-N2", "Status": 1, "Fault": int(fault), "Fan Speed": "40%", "SA Temperature": "32.5°C", "Last Updated": "16/05/2024 01:56 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-N3/{equipment}-INT-N3", "Status": 1, "Fault": 0, "Fan Speed": "64%", "SA Temperature": "32.7°C", "Last Updated": "16/05/2024 01:57 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-W1/{equipment}-INT-W1", "Status": 0, "Fault": int(fault), "Fan Speed": "100%", "SA Temperature": "30.6°C", "Last Updated": "16/05/2024 01:54 PM"},
+            {"Equipment": f"555CS-B-MECH-L02-FCU-W2/{equipment}-INT-W2", "Status": 1, "Fault": 0, "Fan Speed": "57%", "SA Temperature": "32.3°C", "Last Updated": "16/05/2024 01:56 PM"},
+            {"Equipment": f"555CS-B-MECH-L03-FCU-E1/{equipment}-INT-E1", "Status": 1, "Fault": 0, "Fan Speed": "3%", "SA Temperature": "21°C", "Last Updated": "16/05/2024 01:56 PM"},
+            {"Equipment": f"555CS-B-MECH-L03-FCU-E2/{equipment}-INT-E2", "Status": 1, "Fault": 0, "Fan Speed": "42%", "SA Temperature": "21.4°C", "Last Updated": "16/05/2024 01:59 PM"},
+            {"Equipment": f"555CS-B-MECH-L03-FCU-N1/{equipment}-INT-N1", "Status": 1, "Fault": 0, "Fan Speed": "22%", "SA Temperature": "21.2°C", "Last Updated": "16/05/2024 01:59 PM"}
 ], safe=False
 )
+
+import random
+
+def apiWeather(request, value):
+    if value == 1:
+        return JsonResponse({'Weather': 'Sunny', 'temperature': random.randint(100,10000)/100, 'image': '/static/icons/weather-sunny.svg'})
+    elif value == 2:
+        return JsonResponse({'Weather': 'Cloudy', 'temperature': random.randint(100,10000)/100, 'image': '/static/icons/weather-cloudy.svg'})
+    elif value == 3:
+        return JsonResponse({'Weather': 'Rain', 'temperature': random.randint(100,10000)/100, 'image': '/static/icons/weather-rain.svg'})
+    else:
+        return JsonResponse({'Weather': 'Unknown', 'temperature': random.randint(100,10000)/100})
